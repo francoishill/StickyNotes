71673b701ba3274f1003b2bf18c53f553d4f154c
francois <francois@2d6c89da-5cd6-c742-a335-7d0c81585162>
2012-11-28 14:58:36 +0000

    Added AutoUpdating.
    git-svn-id: svn://localhost/stickynotes/trunk@11 2d6c89da-5cd6-c742-a335-7d0c81585162

--- StickyNotes/App.xaml.cs
+++ StickyNotes/App.xaml.cs
@@ -12,5 +12,11 @@ namespace StickyNotes
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			SharedClasses.AutoUpdating.CheckForUpdates(null, null, true);
+		}
 	}
 }
